fda09972b438f5f1f65a88a20f206e6830fbe038
Ali Rasim Kocal <arkocal@gmail.com>
2019-07-16 15:27:46 +0200

    models: Update hostname fiel size in connection bindings
    The current size (20) was enough for docker, but kubernetes hostnames are longer becasue they follow the name.servicename format
    
    Signed-off-by: Ali Rasim Kocal <arkocal@gmail.com>

--- public-interface/iot-entities/postgresql/models/connectionBindings.js
+++ public-interface/iot-entities/postgresql/models/connectionBindings.js
@@ -34,7 +34,7 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: false
         },
         server: {
-            type: DataTypes.STRING(20),
+            type: DataTypes.STRING(50),
             allowNull: false
         },
         connectingStatus: {
