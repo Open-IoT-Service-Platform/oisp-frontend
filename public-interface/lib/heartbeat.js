686782eeb1fa4d2f01cdf775779873153ef1c061
Marcel Wagner <wagmarcel@web.de>
2020-02-23 16:35:09 +0100

    Rework of Kafka usage. Changed heartbeat, rule-updater and metrics-producer * Removed kafka-node since not all exceptions can be caught meaninfully and thus is crashing app * Moved all to kafkajs module which is catching all exceptions and behaves stable when kafka-leaders are failing * Removed the repeated update messaging from rule-updater - one signal is sufficient on kafka * Added configs to set replicationFactor and partitions of topic-partitions for HA mode * Added configs for retries and connnection timeout in case kafka broker is not responsive * Due to practical consideration moved back to forward samples in an array to kafka. Otherwise we risk loosing samples or creating a too high delay until all is sent. Only exception is bytearray types, these are always forwarded as single smaple.
    Signed-off-by: Marcel Wagner <wagmarcel@web.de>

--- public-interface/lib/heartbeat.js
+++ public-interface/lib/heartbeat.js
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2014 Intel Corporation
+ * Copyright (c) 2014-2020 Intel Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,61 +13,71 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+/* esversion: 8  */
 'use strict';
-var Kafka = require('kafka-node'),
+var { Kafka, logLevel } = require('kafkajs'),
     config = require('../config'),
     logger = require('./logger').init(),
     rulesUpdateNotifier = require('../engine/api/helpers/rules-update-notifier'),
     heartBeatInterval = null;
-var kafkaClient = null;
 var kafkaProducer = null;
+var kafkaAdmin = null;
 
-var heartBeat = function(producer, partition, topic) {
-    if ( producer != null) {
-        try {
-            console.log("--------------------Sending heartbeat ...");
-            producer.send([
-                {
-                    topic: topic,
-                    partition: partition,
-                    messages: "dashboard"
-                }
-            ], function (err) {
-                if (err) {
-                    logger.error("Error when sending heartbear message to Kafka: " + JSON.stringify(err));
-                }
-            });
-        } catch(exception) {
-            logger.error("Exception occured when sending heartbear message to Kafka: " + exception);
-        }
-    }
+const heartbeat = (producer, partition, topic) => {
+    return producer
+        .send({
+            topic,
+            messages: [{key: "heartbeat", value:"dashboard", partition: 0}]
+        })
+        .then(console.log("--------------------Sending heartbeat ..."))
+        .catch(async (e) => {
+            logger.error("Error while sending to topic " + topic + " error: " + e);
+            await kafkaProducer.disconnect();
+        });
 };
 
 exports.start = function () {
+    var brokers = config.drsProxy.kafka.uri.split(',');
+    const kafka = new Kafka({
+        logLevel: logLevel.INFO,
+        brokers: brokers,
+        clientId: 'frontend-heartbeat',
+        requestTimeout: config.drsProxy.kafka.requestTimeout,
+        retry: {
+            maxRetryTime: config.drsProxy.kafka.maxRetryTime,
+            retries: config.drsProxy.kafka.retries
+        }
+    });
+    kafkaProducer = kafka.producer();
+    kafkaAdmin    = kafka.admin();
+    const { CONNECT, DISCONNECT} = kafkaProducer.events;
 
-    kafkaClient = new Kafka.KafkaClient({kafkaHost: config.drsProxy.kafka.uri});
-
-    kafkaProducer = new Kafka.HighLevelProducer(kafkaClient, { requireAcks: 1, ackTimeoutMs: 500 });
-
-    kafkaProducer.on('ready', function () {
+    const run = async () => {
         var topic = config.drsProxy.kafka.topicsHeartbeatName;
         var interval = parseInt(config.drsProxy.kafka.topicsHeartbeatInterval);
         var partition = 0;
+        var replicationFactor = config.drsProxy.kafka.replication;
+        try {
+            await kafkaProducer.connect();
+            await kafkaAdmin.createTopics({
+                topics: [{topic: topic, replicationFactor, numPartitions: 1}]
+            });
+        } catch (error) {
+            return logger.error("Error while connecting to channel and creating topic: " + error);
+        }
+        heartBeatInterval = setInterval( function (producer, partition, topic) {
+            heartbeat(producer, partition, topic);
+        }, interval, kafkaProducer, partition, topic );
+        rulesUpdateNotifier.notify();
+    };
 
-        /*jshint -W098 */
-        kafkaProducer.createTopics([topic], true, function (error, data) {
-            if (!error) {
+    run().catch(e => console.error("Kafka runtime error " + e));
 
-                heartBeat(kafkaProducer, partition, topic);
-                heartBeatInterval = setInterval( function (producer, partition, topic) {
-                    heartBeat(producer, partition, topic);
-                }, interval, kafkaProducer, partition, topic );
-                rulesUpdateNotifier.notify();
-            }
-        });
-        /*jshint +W098 */
+    kafkaProducer.on(DISCONNECT, e => {
+        console.log(`Disconnected!: ${e.timestamp}`);
+        kafkaProducer.connect();
     });
+    kafkaProducer.on(CONNECT, e => logger.debug("Kafka heartbeat producer connected: " + e));
 };
 
 exports.stop = function () {
