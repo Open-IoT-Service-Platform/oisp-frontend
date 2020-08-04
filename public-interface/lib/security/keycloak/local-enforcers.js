c6ae36af22809a9918397cad056d400d3684279c
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2020-08-04 10:49:05 +0300

    Keycloak: Added token audience check and local enforcers for device endpoints
    Signed-off-by: Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>

--- public-interface/lib/security/keycloak/local-enforcers.js
+++ public-interface/lib/security/keycloak/local-enforcers.js
@@ -15,17 +15,122 @@
  */
 
 'use strict';
-const errBuilder = require('./../../errorHandler').errBuilder,
+const clientId = require('./config').getKeycloakConfig().resource,
+    errBuilder = require('./../../errorHandler').errBuilder,
     notAuthorizedCode = errBuilder.Errors.Generic.NotAuthorized.code,
-    notAuthorizedMessage = errBuilder.Errors.Generic.NotAuthorized.message;
+    notAuthorizedMessage = errBuilder.Errors.Generic.NotAuthorized.message,
+    clientRoles = {
+        user: "user",
+        sysadmin: "sysadmin",
+        system: "system"
+    },
+    tokenTypes = {
+        user: "user",
+        device: "device"
+    },
+    accountRoles = {
+        admin: "admin",
+        user: "user"
+    };
 
-function deviceAccessEnforcer(req, res, next) {
-    if (!req.tokenInfo || !req.tokenInfo.header || req.tokenInfo.payload.type !== 'device') {
+// Helper functions for local enforcers
+
+function isTokenValid(req) {
+    if (!req.tokenInfo || !req.tokenInfo.header) {
+        return false;
+    }
+    return true;
+}
+
+function hasTokenClientRole(req, clientRole) {
+    if (!isTokenValid(req)) {
+        return false;
+    }
+    if (!req.tokenInfo.payload['resource_access'][clientId] ||
+        !req.tokenInfo.payload['resource_access'][clientId].roles) {
+        return false;
+    }
+    if (req.tokenInfo.payload['resource_access'][clientId].roles.includes[clientRole]) {
+        return true;
+    }
+    return false;
+}
+
+function hasAccessToAccount(req) {
+    if (!isTokenValid(req)) {
+        return false;
+    }
+    if (!req.params.accountId && req.tokenInfo.payload.type === tokenTypes.device) {
+        return true;
+    }
+    if (req.params.accountId && req.tokenInfo.payload.type === tokenTypes.user) {
+        return req.tokenInfo.payload.accounts
+            .some(account => account.id === req.params.accountId);
+    }
+    return false;
+}
+
+function isAdminOfAccount(req) {
+    if (!isTokenValid(req)) {
+        return false;
+    }
+    if (!req.params.accountId && req.tokenInfo.payload.type === tokenTypes.device) {
+        return true;
+    }
+    if (req.params.accountId && req.tokenInfo.payload.type === tokenTypes.user) {
+        return req.tokenInfo.payload.accounts
+            .some(account => account.id === req.params.accountId && account.role === accountRoles.admin);
+    }
+    return false;
+}
+
+// Local enforcer functions
+
+function apiPublicAccess(req, res, next) {
+    return next();
+}
+
+function authReadAccess(req, res, next) {
+    if (isTokenValid(req)) {
+        return next();
+    }
+    return res.status(notAuthorizedCode.send(notAuthorizedMessage));
+}
+
+function catalogReadAccess(req, res, next) {
+    if (hasTokenClientRole(req, clientRoles.sysadmin) ||
+        hasTokenClientRole(req, clientRoles.system) || hasAccessToAccount(req)) {
+        return next();
+    }
+    return res.status(notAuthorizedCode.send(notAuthorizedMessage));
+}
+
+function deviceAdminAccess(req, res, next) {
+    if (isAdminOfAccount(req)){
+        return next();
+    }
+    return res.status(notAuthorizedCode.send(notAuthorizedMessage));
+}
+
+function deviceReadAccess(req, res, next) {
+    if (hasAccessToAccount(req)) {
+        return next();
+    }
+    return res.status(notAuthorizedCode.send(notAuthorizedMessage));
+}
+
+function dataWriteAccess(req, res, next) {
+    if (!isTokenValid(req) || req.tokenInfo.payload.type !== tokenTypes.device) {
         return res.status(notAuthorizedCode).send(notAuthorizedMessage);
     }
     return next();
 }
 
 module.exports = {
-    "data-write": deviceAccessEnforcer
+    "api-public": apiPublicAccess,
+    "auth-read": authReadAccess,
+    "data-write": dataWriteAccess,
+    "catalog-read": catalogReadAccess,
+    "device-admin": deviceAdminAccess,
+    "device-read": deviceReadAccess
 };
